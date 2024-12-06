7b5991295d12a9c608f94045510b81fa3d095cd0
FaizaZahid023 <159225842+FaizaZahid023@users.noreply.github.com>
2024-12-06 22:13:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
 let track_artist = document.querySelector('.track-artist');
 
-let playpause_btn = document.querySelector('.playpause-track');
+let playpause_btn = document.querySelector('.playpause-track')
 let next_btn = document.querySelector('.next-track');
 let prev_btn = document.querySelector('.prev-track');
 
